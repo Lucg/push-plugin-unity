a3f58e85c940eeceb23b824111331afb243f04f5
Nikola Maric <Nikola.Maric@infobip.com>
2014-01-27 16:23:17 +0100

    Resolved conflicts in demo.cs

--- Assets/InfobipPushDemo/InfobipPushDemo.cs
+++ Assets/InfobipPushDemo/InfobipPushDemo.cs
@@ -14,26 +14,6 @@ public class InfobipPushDemo : MonoBehaviour
         labelStyle.normal.textColor = Color.black;
         labelStyle.alignment = TextAnchor.MiddleCenter;
 
-<<<<<<< HEAD
-        infobipPush = new InfobipPush();
-    }
-    
-    void OnGUI()
-    {
-        GUI.Label(new Rect(centerX - 200, 0, 400, 35), "Infobip Push Demo", labelStyle);
-        if (GUI.Button(new Rect(centerX - 175, 40, 150, 35), "Disable Debug Mode"))
-        {
-            infobipPush.setDebugMode(true, 2);
-            // infobipPush.setDebugMode(false);
-            // infobipPush.LogMode = true;
-        }
-        if (GUI.Button(new Rect(centerX + 25, 40, 150, 35), "Enable Debug Mode"))
-        {
-            // infobipPush.LogMode = false;
-        }
-    }
-    
-=======
 		infobipPush = new InfobipPush();
 	}
 	
@@ -69,6 +49,4 @@ public class InfobipPushDemo : MonoBehaviour
             infobipPush.Initialize("063bdab564eb", "a5cf819f36e2");
         }
 	}
-	
->>>>>>> 3b79a56153ded7775097438b26b9ac5a01c146a4
 }
